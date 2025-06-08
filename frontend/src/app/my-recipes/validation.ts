73d794c8daebd04df36c906cca602e87fac63a13
Jason <jason.xh.guan@gmail.com>
2025-06-08 22:19:53 +0100

    Add form fields

---
+++ frontend/src/app/my-recipes/validation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const ingredientSchema = z.object({
+  name: z.string().min(1, { message: "Please enter an ingredient" }),
+  quantity: z.number(),
+  unit: z.enum(["ml", "l", "mg", "g", "kg", "cup", "tbsp", "tsp"]).nullable(),
+});
+
+export const userRecipeSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  description: z.string(),
+  ingredients: z.array(ingredientSchema),
+});
+
+export type UserRecipeValidationType = z.infer<typeof userRecipeSchema>;
